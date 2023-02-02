59f03954de0f2600e091dc31183c5b9387a9ef20
Dorabang <psa1195@naver.com>
2023-02-03 00:55:22 +0900

    feat: Add EmailAuth in firebase

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,4 +1,79 @@
-import React from "react";
+import { authService } from 'fbase';
+import React from 'react';
+import { useState } from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newAccount, setNewAcount] = useState(true);
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+
+    console.log(value);
+
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount === true) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label>
+          email&nbsp;
+          <input
+            type='email'
+            placeholder='Email'
+            name='email'
+            required
+            value={email}
+            onChange={onChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password&nbsp;
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            required
+            value={password}
+            onChange={onChange}
+          />
+        </label>
+        <input type='submit' value={newAccount ? 'Create Acount' : 'Login'} />
+      </form>
+
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
+
 export default Auth;
