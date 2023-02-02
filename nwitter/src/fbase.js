da7d5315b76afeeaa530c906190ea00ed60b1927
Dorabang <psa1195@naver.com>
2023-02-03 01:55:48 +0900

    chore: Get signInWithPopup in Firebase

--- nwitter/src/fbase.js
+++ nwitter/src/fbase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,5 +12,7 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
