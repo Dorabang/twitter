2b56674313bac9d1aefa3613d221c38e3b9dda21
Dorabang <psa1195@naver.com>
2023-02-07 13:20:11 +0900

    feat: Add log-out method

--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -1,18 +1,22 @@
-import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Auth from "routes/Auth";
-import Home from "routes/Home";
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Auth from 'routes/Auth';
+import Home from 'routes/Home';
+import Profile from 'routes/Profile';
+import Navigation from 'components/Navigation';
 
 function AppRouter({ isLoggedIn }) {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
-            <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/profile' element={<Profile />} />
           </>
         ) : (
-          <Route path="/" element={<Auth />} />
+          <Route path='/' element={<Auth />} />
         )}
       </Routes>
     </Router>
--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -1,3 +1,17 @@
-import React from "react";
+import { authService } from 'fbase';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default () => <span>Profile</span>;
+export default () => {
+  const Navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    Navigate('/');
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
